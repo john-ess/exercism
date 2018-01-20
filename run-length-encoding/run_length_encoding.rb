25e045a6d84ade77406429c269ce4141879bbea3
John Serafin <j-serafin@northwestern.edu>
2018-01-19 23:40:45 -0600

    run_lenght-encoding - update encode method w/ reduce

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -1,19 +1,9 @@
-
 class RunLengthEncoding
   def self.encode decoded
-
-    order  = decoded.chars.chunk_while(&:==).map(&:first)
-
-    counts = order.map do |letter|
-                count = 0
-                while decoded[0,1] == letter
-                  decoded.slice!(0)
-                  count += 1
-                end
-                count = count > 1 ? count : 0
-              end
-
-    counts.zip(order).join.to_s.gsub('0','')
+    decoded.chars.chunk_while(&:==).to_a.reduce('') do |encoded, letters|
+      encoded << "#{letters.size}" unless letters.size == 1
+      encoded << "#{letters[0]}"
+    end
   end
 
   def self.decode encoded
