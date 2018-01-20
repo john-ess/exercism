131112c47064870ed8d149a3066a19b01edd4f05
John Serafin <j-serafin@northwestern.edu>
2018-01-20 17:20:11 -0600

    binary - second draft

--- binary/binary.rb
+++ binary/binary.rb
@@ -1,20 +1,10 @@
-# Convert a binary number, represented as a string (e.g. '101010'),
-# to its decimal equivalent using first principles.
-
 class Binary
   def self.to_decimal binary
     raise ArgumentError.new('Binary numbers consist of 1s and 0s only.') unless binary.scan(/([1])|([0])/).join == binary
     return 0 if binary.to_i.zero?
 
-    x = binary.reverse
-    total = 0
-    x.chars.each_with_index { |num, i|
-        total = total + num.to_i * (2 ** i.to_i)
-    }
-    return total
-
+    binary.reverse.chars.each_with_index.map { |num, i| num.to_i * (2 ** i)}.reduce(0,:+)
   end
-
 end
 
 module BookKeeping
