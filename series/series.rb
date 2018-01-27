48c43790f0c4643f96fceff67bfce87cca299e96
John Serafin <j-serafin@northwestern.edu>
2018-01-27 02:29:41 -0600

    series - final

--- series/series.rb
+++ series/series.rb
@@ -5,7 +5,11 @@ class Series
   end
 
   def slices series_length
-    raise ArgumentError.new("#{series_length} > #{@num.length}") if series_length > @num.length
-    @num.chars.each_cons(series_length).map { |set| set.join }
+    raise ArgumentError.new("'#{@num}' is shorter than #{series_length}") if series_length > @num.length
+    # break into array of characters
+    #   get each consecutive set given number of chars
+    #     join the individual chars in that set
+    #       map the result to a returned array
+    @num.chars.each_cons(series_length).map(&:join)
   end
 end
