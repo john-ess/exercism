d1fe3a7e9fe5e2952e0db6e5f488e2e5fb163ffd
John Serafin <j-serafin@northwestern.edu>
2018-02-06 15:38:54 -0600

    space_age - remove Planets module, move planet multiples into hash

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,26 +1,25 @@
 # Dynamic Method: http://rohitrox.github.io/2013/07/02/ruby-dynamic-methods/
 # Loop through constants: https://stackoverflow.com/questions/6712621/ruby-iterating-through-constants
-module Planets
-  EARTH   =  1
-  MERCURY =  0.2408467
-  VENUS   =  0.61519726
-  MARS    =  1.8808158
-  JUPITER =  11.862615
-  SATURN  =  29.447498
-  URANUS  =  84.016846
-  NEPTUNE =  164.79132
-end
 
 class SpaceAge
   EARTH_YEAR_IN_SECONDS = 365.25 * 60 * 60 * 24
+
+  ORBIT_MULTIPLES = { 'earth' =>  1,
+    'mercury' => 0.2408467,
+    'venus'   => 0.61519726,
+    'mars'    => 1.8808158,
+    'jupiter' => 11.862615,
+    'saturn'  => 29.447498,
+    'uranus'  => 84.016846,
+    'neptune' => 164.79132 }
 
   def initialize seconds
     @age_in_seconds = seconds
   end
 
-  Planets.constants.each do |method|
-    define_method "on_#{method.downcase}" do
-      @age_in_seconds / (EARTH_YEAR_IN_SECONDS * Planets.const_get(method.upcase))
+  ORBIT_MULTIPLES.each do |planet, multiple|
+    define_method "on_#{planet}" do
+      @age_in_seconds / (EARTH_YEAR_IN_SECONDS * multiple)
     end
   end
 end
