891fd1826dee7e99f9593c4d4963b15653f98e85
John Serafin <j-serafin@northwestern.edu>
2018-02-06 14:53:32 -0600

    space_age - move Planets to module

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,7 +1,6 @@
-# See: http://rohitrox.github.io/2013/07/02/ruby-dynamic-methods/
-
-class SpaceAge
-  EARTH_YEAR_IN_SECONDS = 365.25 * 60 * 60 * 24
+# Dynamic Method: http://rohitrox.github.io/2013/07/02/ruby-dynamic-methods/
+# Loop through constants: https://stackoverflow.com/questions/6712621/ruby-iterating-through-constants
+module Planets
   EARTH   =  1
   MERCURY =  0.2408467
   VENUS   =  0.61519726
@@ -10,17 +9,20 @@ class SpaceAge
   SATURN  =  29.447498
   URANUS  =  84.016846
   NEPTUNE =  164.79132
+end
+
+class SpaceAge
+  EARTH_YEAR_IN_SECONDS = 365.25 * 60 * 60 * 24
 
   def initialize seconds
     @age_in_seconds = seconds
   end
 
-  [:earth, :mercury, :venus, :mars, :jupiter, :saturn, :uranus, :neptune].each do |method|
-    define_method "on_#{method}" do
-      @age_in_seconds / (EARTH_YEAR_IN_SECONDS * SpaceAge.const_get(method.upcase))
+  Planets.constants.each do |method|
+    define_method "on_#{method.downcase}" do
+      @age_in_seconds / (EARTH_YEAR_IN_SECONDS * Planets.const_get(method.upcase))
     end
   end
-
 end
 
 module BookKeeping
