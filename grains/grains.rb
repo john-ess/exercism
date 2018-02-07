13769ecae8fe9350d361746048ce27cad082d91a
john-ess <j-serafin@northwestern.edu>
2018-02-07 04:47:15 -0600

    grains - clean up

--- grains/grains.rb
+++ grains/grains.rb
@@ -1,11 +1,13 @@
 class Grains
+  CHESS_SQUARES = (1..64)
+
   def self.square num
-    raise ArgumentError.new('Must be a number on a chess board.') unless num >= 1 && num <= 64
+    raise ArgumentError.new('Must be a square on a chess board.') unless CHESS_SQUARES.include? num
     2 ** (num - 1)
   end
 
   def self.total
-    (1..64).reduce { |sum, i| sum += square i }
+    CHESS_SQUARES.reduce { |sum, i| sum += square i }
   end
 end
 
