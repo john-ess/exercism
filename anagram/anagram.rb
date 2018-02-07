6d3a7d5334327a43b5d6411da08e4631e3064b0c
John Serafin <j-serafin@northwestern.edu>
2018-02-06 18:06:56 -0600

    anagram - first draft

--- anagram/anagram.rb
+++ anagram/anagram.rb
@@ -1,3 +1,22 @@
 class Anagram
 
+  def initialize word
+    @word = word
+  end
+
+  def standardize word
+    return word.downcase.chars.sort.join
+  end
+
+  def match check_word_against
+    check_word_against.reject! { |candidate| candidate.downcase == @word.downcase }
+    check_word_against.reject do |candidate|
+      standardize(candidate) != standardize(@word)
+    end
+  end
+
+end
+
+module BookKeeping
+  VERSION = 2 # Where the version number matches the one in the test.
 end
