ff30cb456ba085966f5b0fe62fb3ddfde9009533
john-ess <j-serafin@northwestern.edu>
2018-02-07 22:25:09 -0600

    grains - uses sum method from 2.4.1

--- grains/grains.rb
+++ grains/grains.rb
@@ -1,13 +1,15 @@
 class Grains
-  CHESS_SQUARES = (1..64)
+  FIRST = 1
+  LAST  = 64
 
   def self.square num
-    raise ArgumentError.new('Must be a square on a chess board.') unless CHESS_SQUARES.include? num
+    raise ArgumentError.new('Must be a square on a chess board.') unless num.between?(FIRST, LAST)
     2 ** (num - 1)
   end
 
   def self.total
-    CHESS_SQUARES.reduce { |sum, i| sum += square i }
+    (FIRST..LAST).sum(&method(:square))              # ruby 2.4.1
+    # CHESS_SQUARES.reduce{ |sum, i| sum += square i } ruby 2.3.1
   end
 end
 
